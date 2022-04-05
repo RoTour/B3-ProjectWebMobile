fd4c725eedad13ff0496ac86b5d4cf6eb76e41f1
rotour <rothewawa@gmail.com>
2022-04-05 09:35:02 +0200

    add:  Register form, select between login and register

--- apps/mobile/src/app/App.tsx
+++ apps/mobile/src/app/App.tsx
@@ -1,7 +1,4 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
-
-import { Colors } from 'react-native/Libraries/NewAppScreen';
 // @ts-ignore
 import openURLInBrowser from 'react-native/Libraries/Core/Devtools/openURLInBrowser';
 import { NavigationContainer } from '@react-navigation/native';
@@ -20,83 +17,11 @@ const App = () => {
           component={Login}
           options={{
             headerStyle: { backgroundColor: "#080C1E" },
-            headerTitleStyle: { color: "#fff", fontSize: 30, fontWeight: "bold" },
+            headerTitleStyle: { color: "#ffffff00", fontSize: 0, fontWeight: "bold" },
         }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  header: {
-    backgroundColor: '#143055',
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingVertical: 24,
-  },
-  logo: {
-    width: 200,
-    height: 180,
-    resizeMode: 'contain',
-  },
-  heading: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.lighter,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-  link: {
-    color: '#45bc98',
-  },
-  githubStarContainer: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    padding: 10,
-  },
-  githubStarBadge: {
-    borderWidth: 1,
-    borderColor: Colors.dark,
-    borderRadius: 3,
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    paddingHorizontal: 10,
-    paddingVertical: 3,
-    fontWeight: '600',
-  },
-});
 
 export default App;
--- apps/mobile/src/app/components/pages/Login/Login.tsx
+++ apps/mobile/src/app/components/pages/Login/Login.tsx
@@ -1,49 +1,118 @@
 import React, { FC, useState } from 'react';
-import { SafeAreaView, StatusBar, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+import { SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 type LoginProps = Record<string, never>;
 
 const Login: FC<LoginProps> = () => {
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
+  const [confirmPassword, setConfirmPassword] = useState<string>('');
+  const [error, setError] = useState<string>('');
+
+
+  const [route, setRoute] = useState<'login' | 'register'>('login');
+
+  const changeRoute = (newRouter: 'login' | 'register') => {
+    setError('');
+    setRoute(newRouter);
+  };
 
   const handleLogin = () => {
     console.log('email: ', email);
     console.log('password: ', password);
   };
 
+  const handleRegister = () => {
+    setError('');
+    console.log('email: ', email);
+    console.log('password: ', password);
+    console.log('confirmPassword: ', confirmPassword);
+    if (password !== confirmPassword) {
+      setError('Passwords do not match');
+    }
+  };
+
   return <SafeAreaView style={ styles.container }>
-      <View style={styles.form}>
-        <Text style={styles.label}>Login</Text>
-        <View>
-          <TextInput
-            placeholder="Email"
-            value={ email }
-            style={ styles.input }
-            onChangeText={ setEmail }/>
-        </View>
-        <Text style={styles.label}>Password</Text>
-        <View>
-          <TextInput
-            placeholder={ 'Password' }
-            secureTextEntry={ true }
-            value={ password }
-            style={ styles.input }
-            onChangeText={ setPassword }/>
-        </View>
-        <View>
-          <TouchableOpacity style={ styles.loginScreenButton } onPress={ handleLogin }>
-            <Text style={ styles.loginBtnTxt }>Login</Text>
-          </TouchableOpacity>
-        </View>
+    <View style={ [styles.displayFlex, styles.margin] }>
+      <Text style={ [styles.selectForm, styles.route, route === 'login' ? styles.activeRoute : styles.inactive] }
+            onPress={ () => changeRoute('login') }>Login</Text>
+      <Text style={ [styles.selectForm, styles.route, route === 'register' ? styles.activeRoute : styles.inactive] }
+            onPress={ () => changeRoute('register') }>Register</Text>
+    </View>
+    <View style={[styles.margin, !error ? styles.hidden : null]}>
+      <Text style={ styles.error }>{ error }</Text>
+    </View>
+    <View style={ styles.form }>
+      <Text style={ styles.label }>Login</Text>
+      <View>
+        <TextInput
+          placeholder="Email"
+          value={ email }
+          style={ styles.input }
+          onChangeText={ setEmail }/>
       </View>
-    </SafeAreaView>;
+      <Text style={ styles.label }>Password</Text>
+      <View>
+        <TextInput
+          placeholder={ 'Password' }
+          secureTextEntry={ true }
+          value={ password }
+          style={ styles.input }
+          onChangeText={ setPassword }/>
+      </View>
+      <Text style={ [styles.label, route !== 'register' ? styles.hidden : null] }>Retype your password</Text>
+      <View>
+        <TextInput
+          placeholder={ 'Password' }
+          secureTextEntry={ true }
+          value={ confirmPassword }
+          style={ [styles.input, route !== 'register' ? styles.hidden : null] }
+          onChangeText={ setConfirmPassword }/>
+      </View>
+      <View>
+        <TouchableOpacity style={ styles.loginScreenButton }
+                          onPress={ route === 'login' ? handleLogin : handleRegister }>
+          <Text style={ styles.loginBtnTxt }>{ route === 'login' ? 'Login' : 'Register' }</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  </SafeAreaView>;
 };
 
 export default Login;
 
 const styles = StyleSheet.create({
+  displayFlex: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  hidden: {
+    display: 'none',
+  },
+  margin: {
+    marginLeft: 20,
+    marginRight: 20,
+  },
+  route: {
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  activeRoute: {
+    color: '#44d62c',
+    textDecorationStyle: 'solid',
+    textDecorationColor: '#44d62c',
+    textDecorationLine: 'underline',
+  },
+  error: {
+    marginTop: 20,
+    color: 'red',
+    fontWeight: 'bold',
+    fontSize: 15,
+  },
+  inactive: {},
   container: {
     flex: 1,
     backgroundColor: '#080C1E',
@@ -52,9 +121,12 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     padding: 20,
   },
+  selectForm: {
+    color: '#fff',
+  },
   label: {
     color: '#fff',
-    fontSize: 20,
+    fontSize: 15,
     marginBottom: 10,
     fontWeight: 'bold',
   },
@@ -66,7 +138,7 @@ const styles = StyleSheet.create({
     marginBottom: 10,
   },
   button: {
-    color: '#0f0',
+    color: '#44d62c',
   },
   loginBtnTxt: {
     color: '#080C1E',
@@ -84,7 +156,6 @@ const styles = StyleSheet.create({
   },
   form: {
     margin: 20,
-    flex: 1,
     alignItems: 'stretch',
     justifyContent: 'center',
   },
