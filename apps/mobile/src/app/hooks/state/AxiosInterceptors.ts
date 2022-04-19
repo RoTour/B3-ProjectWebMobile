579175021ce7fbfca9f5f5c4d8523ef5ecce0920
rotour <rothewawa@gmail.com>
2022-04-19 11:54:20 +0200

    fix: disconnection when logged out once

--- apps/mobile/src/app/hooks/state/AxiosInterceptors.ts
+++ apps/mobile/src/app/hooks/state/AxiosInterceptors.ts
@@ -1,5 +1,6 @@
 import create from 'zustand';
 import axios from 'axios';
+import { JwtHeaderInterceptor } from '../../interceptors/jwt-header.interceptor';
 
 type AxiosInterceptorsState = {
   interceptors: number[];
@@ -13,12 +14,14 @@ export const useAxiosInterceptors = create<AxiosInterceptorsState>((set, get) =>
     interceptors: array,
     addInterceptor: (interceptorId: number) => set({ interceptors: [...get().interceptors, interceptorId] }),
     clearInterceptors: () => {
+      console.log(`clearing interceptors (requests: ${ axios.interceptors.request })`);
       get().interceptors.forEach(interceptorId => {
         axios.interceptors.request.eject(interceptorId);
       });
       get().interceptors.forEach(interceptorId => {
         axios.interceptors.response.eject(interceptorId);
       });
+      JwtHeaderInterceptor();
     },
   };
 });
