59bd07584b04d17c9b8617f43bf21aabd7fabdce
kopenkinda <kopenkin.da@gmail.com>
2022-05-02 13:54:44 +0200

    feat(Swagger): :memo: Added the Swagger UI (OpenAPI Spec) Docs set-up

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,13 +5,16 @@
 
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import cors from 'cors';
 import { AppModule } from './app/app.module';
 
+const globalPrefix = 'api';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+
   app.useGlobalPipes(new ValidationPipe());
   app.use(
     cors({
@@ -19,6 +22,19 @@ async function bootstrap() {
     })
   );
   const port = process.env.PORT || 3333;
+
+  const config = new DocumentBuilder()
+    .setTitle('WhatsUpp')
+    .setDescription(
+      'The Chat API description. (Final project of the 3rd year @ YNOV)'
+    )
+    .setVersion('0.1')
+    .addTag('chat')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
